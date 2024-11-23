951cadda39262023d73714b2d4bab40d8b3e3e3a
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2024-11-23 11:14:45 +0000

    Deploying to gh-pages from @ Mukuta0Pusmenski/goit-js-hw-10@1f47b0c6551beb3a14f4de57b7ca760e8bab4ca7 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,2 +1,2 @@
 import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const n=document.querySelector(".form");n.addEventListener("submit",function(e){e.preventDefault();const i=n.elements.delay,r=n.elements.state;let t=Number(i.value);const o=r.value;t<1500&&(t=1500),c(t,o).then(s=>{iziToast.success({title:"Success",message:`✅ Fulfilled promise in ${s}ms`})}).catch(s=>{iziToast.error({title:"Error",message:`❌ Rejected promise in ${s}ms`})}),i.value="",r.forEach(s=>s.checked=!1)});function c(e,i){return new Promise((r,t)=>{setTimeout(()=>{i==="fulfilled"?r(e):t(e)},e)})}
-//# sourceMappingURL=2-cnackbar.js.map
+//# sourceMappingURL=2-snackbar.js.map
